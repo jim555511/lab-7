bc21d3b2548bc5920c3a34b9876e699b64eef079
Stoy, Daniel (stoyda) <stoyda@mail.uc.edu>
2019-10-22 10:10:03 -0400

    Basic boilerplate

--- lab7TreeDLL/lab7TreeDLL/lab7TreeDLL.cpp
+++ lab7TreeDLL/lab7TreeDLL/lab7TreeDLL.cpp
@@ -3,19 +3,93 @@
 
 #include "framework.h"
 #include "lab7TreeDLL.h"
+#include <string>
+using namespace std;
 
-
-// This is an example of an exported variable
-LAB7TREEDLL_API int nlab7TreeDLL=0;
-
-// This is an example of an exported function.
-LAB7TREEDLL_API int fnlab7TreeDLL(void)
-{
-    return 0;
+Node::Node() {
+	val = "";
+	left = nullptr;
+	right = nullptr;
 }
 
-// This is the constructor of a class that has been exported.
-Clab7TreeDLL::Clab7TreeDLL()
-{
-    return;
+//Empty for now until we've used it a bit
+Node::~Node() {
 }
+
+Node::Node(string Val) {
+	val = Val;
+	left = nullptr;
+	right = nullptr;
+}
+
+Node::Node(Node* Left, Node* Right, string Val) {
+	left = Left;
+	right = Right;
+	val = Val;
+}
+
+Node* Node::getLeft() {
+	return left;
+}
+
+Node* Node::getRight() {
+	return right;
+}
+
+string Node::getVal() {
+	return val;
+}
+
+void Node::setLeft(Node* Left) {
+	left = Left;
+}
+
+void Node::setRight(Node* Right) {
+	right = Right;
+}
+
+void Node::setVal(string Val) {
+	val = Val;
+}
+
+BinaryTree::BinaryTree() {
+
+}
+
+BinaryTree::BinaryTree(Node* Top) {
+	top = Top;
+}
+
+BinaryTree::~BinaryTree() {
+
+}
+
+bool BinaryTree::insert(string valToInsert) {
+	return false;
+}
+
+Node* BinaryTree::Find(string valToFind) {
+	return nullptr;
+}
+
+int BinaryTree::Size() {
+	return 0;
+}
+
+Node* BinaryTree::getAllAscending() {
+	return nullptr;
+}
+
+Node* BinaryTree::getAllDescending() {
+	return nullptr;
+}
+
+bool BinaryTree::EmptyTree() {
+	return false;
+}
+
+bool BinaryTree::remove(string valToRemove) {
+	return false;
+}
+
+
--- lab7TreeDLL/lab7TreeDLL/lab7TreeDLL.h
+++ lab7TreeDLL/lab7TreeDLL/lab7TreeDLL.h
@@ -9,12 +9,50 @@
 #else
 #define LAB7TREEDLL_API __declspec(dllimport)
 #endif
+#include <string>
 
-// This class is exported from the dll
+//Basic node class for tree with all basic utilities
+class LAB7TREEDLL_API Node {
+private:
+	Node* left;
+	Node* right;
+	std::string val;
+
+public:
+	Node(Node* Left, Node* Right, std::string Val);
+	Node(std::string Val);
+	Node();
+	void setVal(std::string Val);
+	void setLeft(Node* Left);
+	void setRight(Node* Right);
+	std::string getVal();
+	Node* getLeft();
+	Node* getRight();
+
+	virtual ~Node();
+
+};
+
+// Some basic Rules for the tree:
+//1.) The first object put in is our middle, objects that go to the left are smaller,
+//and objects that go to the right are bigger
+//2.) Objects inserted after should always go to the left or right of the next object 
+//depending on if it's bigger or smaller, this makes it easy to search for items
 class LAB7TREEDLL_API BinaryTree {
 public:
-private:
+	BinaryTree(Node* Top); //Constructor if we have a node to start with
+	BinaryTree(); //default constructor
+	~BinaryTree();
+	bool insert(std::string valToInsert);
+	Node * Find(std::string valToFind);
+	int Size();
+	Node* getAllAscending();
+	Node* getAllDescending();
+	bool EmptyTree();
+	bool remove(std::string valToRemove);
 
+private:
+	Node* top; //our top node
 };
 
 
@@ -38,6 +76,3 @@ private:
 ░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░
 ░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░*/
 
-extern LAB7TREEDLL_API int nlab7TreeDLL;
-
-LAB7TREEDLL_API int fnlab7TreeDLL(void);
